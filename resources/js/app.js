888c21e8f444c34b629cfb0b10cc99c5237a6859
Eosia <kevin.eosiaweb@gmail.com>
2025-03-17 22:39:34 +0100

    Mise en place du projet

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,2 @@
 import './bootstrap';
+import htmx from 'htmx.org';
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+
+Route::get('/first-test', function () {
+    return "Coucou HTMX";
 });
